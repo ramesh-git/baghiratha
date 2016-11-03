1ca82021264258d524f53d917fc84048b735eeea
Ramesh <bramesh.mca07@gmail.com>
2016-11-03 22:43:45 +0530

    Update appIndex.js

--- appIndex.js
+++ appIndex.js
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+$("#log").append("in js file");
 var masterServiceBaseURL = "/tdwsp/mastersServices/";
 $(document).ready(function () {
  $('#submit').click(function (event) {
